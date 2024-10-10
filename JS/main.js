9ed65986664babcd6e6caf406b0fbba7faf1f0e9
Ayham <ayhamhassouneh@gmail.com>
2024-10-10 18:17:30 +0300

    finished header

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,31 @@
 import "../styles/style.css";
+import "../styles/header.css";
+
+/*====== show menu =====*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*  menu show  */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*===== remove menu mobile =====*/
+const navLink =document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu= document.getElementById('nav-menu')
+    // when we click on each link. we remove the show menu 
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
