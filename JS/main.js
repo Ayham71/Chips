a1056c91f2eac2aa223c91cda1d454392ce69f46
Ayham <ayhamhassouneh@gmail.com>
2024-10-13 01:34:11 +0300

    finished footer

--- JS/main.js
+++ JS/main.js
@@ -6,6 +6,7 @@ import "../styles/swiper-bundle.min.css";
 import "../styles/care-banner.css";
 import "../styles/products.css";
 import "../styles/contact.css";
+import "../styles/footer.css";
 
 /*====== show menu =====*/
 const navMenu = document.getElementById('nav-menu'),
@@ -45,10 +46,47 @@ const shadowHeader =() =>{
 } 
 window.addEventListener('scroll', shadowHeader)
 
-/*===== swiper favorite  =====*/
-/*const swiperFavorites = new Swiper('favorites__swiper', {
+         /*===== swiper favorite  =====*/
+
+const swiperFavorites = new Swiper('favorites__swiper', {
     loop: true,
     grabCursor: true, 
     slidesPreView: 'auto', 
     centeredSlides: 'auto', 
-  })*/
+  })
+
+
+
+     /*==========   Show scroll up   ==========*/
+
+const scrollUp =() =>{
+    const scroolUp = document.getElementById('scroll-up')
+    // when the scroll is higher than 350 viewpoint height,
+    // add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        :  scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+
+   /*==========    Scroll sections active link    ==========*/
+ 
+   const sections = document.querySelectorAll('section[id]')
+
+   const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(curret =>{
+        const sectionHeight = curret.offsetHeight,
+              sectionTop = curret.offsetTop - 58,
+              sectionId = curret.gotAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[herf*=' + sectionId + ']')
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        } else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+   }
+window.addEventListener('scroll', scrollActive)
